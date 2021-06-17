daa1449575c6c41919d9c7f3d9185efb14f27dba
Daren Hayward <darhaywa@synamedia.com>
2021-06-17 08:56:32 +0100

    Initial Commit

---
+++ SnapshotIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapshotIssue
+//
+//  Created by Daren Hayward on 17/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
